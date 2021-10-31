81591fd8cb62baafef539cdda92b625b08e1bcfb
moeen.tavakoli <mtawakoli2283@gmail.com>
2021-10-31 15:52:00 +0330

    set hash function into signup and login

--- db/user.js
+++ db/user.js
@@ -1,5 +1,7 @@
 const client = require("./connection")
 
+const {hash} = require("../utilities/hash/bcrypt")
+
 client.connect()
 
 
@@ -9,24 +11,29 @@ async function getAllUser() {
 }
 
 async function signup(username, password) {
-    const result = await client.query("INSERT INTO users(username, password) VALUES($1, $2)", [username, password])
+    const result = await client.query("INSERT INTO users(username, password) VALUES($1, $2)", [username, await hash(password)])
     if(result.rowCount != 0){
         console.log("true");
     }
     else console.log("false");
 }
 
+
+// req.pass , username , pass 
 async function login(username, password) {
-    const result = await client.query("SELECT * FROM users WHERE username = $1 AND password = $2", [username, password])
+    const result = await client.query("SELECT * FROM users WHERE username = $1 AND password = $2 LIMIT 1", [username,await hash(password)])
+     
     if(result.rows.length != 0){
-        console.log("true");
+        return true
     }
-    else console.log("false");
+    else return true
 }
 
 
 
-// login('moein2','test')
-// getAllUser()
-// signup("moein2" , "test")
 
+module.exports = {
+    signup,
+    login,
+    getAllUser
+}
