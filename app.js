ac018ec85c33c2b7a156b2c746109561c043387e
moeen.tavakoli <mtawakoli2283@gmail.com>
2021-11-01 07:32:19 +0330

    implemnet signup login in api but not clean code

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 require("./db/connection")
+
+// badan pak beshe 
+const {login , signup , getAllUser} = require("./db/user")
+
+
+app.post('/user/signup',async (req, res) => {
+    const {username  , password } = req.body
+    const result =await signup(username , password)
+    res.send(result)
+})
+
+
+
+app.post('/user/login',async (req, res) => {
+    const {username  , password } = req.body
+    const result =await login(username , password)
+    res.send(result)
+})
+
+
+app.get('/users',async (req, res) => {
+    
+    res.send(await getAllUser())
+})
+
+
+
+app.listen(port = 3000, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
--- db/user.js
+++ db/user.js
@@ -1,32 +1,42 @@
 const client = require("./connection")
 
-const {hash} = require("../utilities/hash/bcrypt")
+const { hash } = require("../service/hash/bcrypt")
 
 client.connect()
 
 
 async function getAllUser() {
     const res = await client.query("SELECT * FROM users")
-    console.log(res.rows);
+    return res.rows;
 }
 
 async function signup(username, password) {
-    const result = await client.query("INSERT INTO users(username, password) VALUES($1, $2)", [username, await hash(password)])
-    if(result.rowCount != 0){
-        console.log("true");
+    try {
+        const result = await client.query("INSERT INTO users(username, password) VALUES($1, $2)", [username, await hash(password)])
+        if (result.rowCount != 0) {
+            return true
+        }
+        else return false
     }
-    else console.log("false");
+    catch (err) {
+        return false
+    }
 }
 
 
 // req.pass , username , pass 
 async function login(username, password) {
-    const result = await client.query("SELECT * FROM users WHERE username = $1 AND password = $2 LIMIT 1", [username,await hash(password)])
-     
-    if(result.rows.length != 0){
-        return true
+    try {
+        const result = await client.query("SELECT * FROM users WHERE username = $1 AND password = $2 LIMIT 1", [username, await hash(password)])
+        if (result.rows.length != 0) {
+            return true
+        }
+        else return false
     }
-    else return true
+    catch (err) {
+        console.log(err);
+        return false
+    }
 }
 
 
